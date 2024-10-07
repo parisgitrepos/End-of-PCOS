2ecca83d46878e4f835f212159aadce119c589b2
parisgitrepos <125218271+parisgitrepos@users.noreply.github.com>
2024-10-07 18:20:29 -0400

    Minor edit

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app.secret_key = env.get("APP_SECRET_KEY")
 
 oauth = OAuth(app)
 oauth.register("auth0", client_id=env.get("AUTH0_CLIENT_ID"), client_secret=env.get("AUTH0_CLIENT_SECRET"),
-               client_kwargs={"scope": "openid profile email", },
+               client_kwargs={"scope": "openid profile email"},
                server_metadata_url=f'https://{env.get("AUTH0_DOMAIN")}/.well-known/openid-configuration')
 
 
